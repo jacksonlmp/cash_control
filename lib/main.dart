67fa2fff09b89e2f4eab02414eb69264595625be
jacksonlmp <jacksonlmp17.jl@gmail.com>
2025-04-02 21:29:13 -0300

    Main com tela de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,31 @@
+import 'package:cash_control/ui/view_model/register_view_model.dart';
+import 'package:cash_control/ui/widgets/register.screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'data/repositories/user_repository_impl.dart';
+import 'data/services/user_services.dart';
 
 void main() {
-  runApp(const MyApp());
+  final repository = UserRepositoryImpl();
+  final service = UserService(repository);
+  final viewModel = RegisterViewModel(service);
+
+  runApp(MyApp(viewModel));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final RegisterViewModel viewModel;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const MyApp(this.viewModel, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return ChangeNotifierProvider.value(
+      value: viewModel,
+      child: MaterialApp(
+        title: 'Cadastro',
+        home: const RegisterScreen(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
