f19203db1d66e1579b8e06ff4481308a02fcbd61
jacksonlmp <jacksonlmp17.jl@gmail.com>
2025-04-13 02:13:45 -0300

    Tela de boas vindas

---
+++ lib/ui/widgets/welcome.screen.dart
@@ -0,0 +1,65 @@
+// lib/ui/user/widgets/welcome_screen.dart
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo_cash_control.jpg',
+                width: 300,
+                height: 300,
+              ),
+
+              const SizedBox(height: 8),
+              Column(
+                children: const [
+                  Text(
+                    'O jeito mais fácil de controlar suas',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  Text(
+                    'FINANÇAS',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: () {
+                  // Navegação será adicionada posteriormente
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.purple,
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 15,
+                  ),
+                ),
+                child: const Text('Próximo', style: TextStyle(fontSize: 16)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
